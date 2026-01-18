b41f2018e4eccff12eeeaba3acbb499c225a2926
MehmetY18 <mehmet.yucel.18.tr@gmail.com>
2026-01-18 14:10:08 +0300

    SLM eklendi

--- AracKiralama/Form1.cs
+++ AracKiralama/Form1.cs
@@ -20,6 +20,7 @@ namespace AracKiralama
         private void Form1_Load(object sender, EventArgs e)
         {
             Console.WriteLine("Merhaba");
+            Console.WriteLine("SLM");
         }
     }
 }
